035e40152944df841ca522dfa54a106d1c76e879
spickfeet <slavaspickfeet@mail.ru>
2024-07-24 22:39:28 +0700

    Kuga
    Kuga

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _cameraTarget;
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _offset;
+
+    private void Update()
+    {
+        Move();
+    }
+    public void Move()
+    {
+        transform.position = Vector3.Lerp(transform.position, _cameraTarget.position + _offset, Time.deltaTime * _speed);
+    }
+}
